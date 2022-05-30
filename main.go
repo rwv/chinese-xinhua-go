02b98f22f15b22b925c614421b9fe0049e3e5d57
Seedgou <i@zczc.cz>
2022-05-30 08:53:55 +0000

    add main

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/rwv/chinese-xinhua-go/ci"
+	"github.com/rwv/chinese-xinhua-go/idiom"
+	"github.com/rwv/chinese-xinhua-go/word"
+	"github.com/rwv/chinese-xinhua-go/xiehouyu"
+)
+
 func main() {
-	println("Hello, world!")
+	idioms, err := idiom.GetIdioms()
+	if err == nil {
+		fmt.Printf("成语: %d\n", len(idioms))
+	}
+
+	words, err := word.GetWords()
+	if err == nil {
+		fmt.Printf("汉字: %d\n", len(words))
+	}
+
+	cis, err := ci.GetCis()
+	if err == nil {
+		fmt.Printf("词语: %d\n", len(cis))
+	}
+
+	xiehouyus, err := xiehouyu.GetXieHouYus()
+	if err == nil {
+		fmt.Printf("歇后语: %d\n", len(xiehouyus))
+	}
 }
